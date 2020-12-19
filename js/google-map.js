ef2930c92eb9f223a12a208d51d1a44037f7ecc3
AdithyaP7 <54159454+AdithyaP7@users.noreply.github.com>
2020-12-18 22:41:29 -0800

    pushed tabbed layout

---
+++ js/google-map.js
@@ -0,0 +1,226 @@
+function initMap() {
+
+    var givenLatitude = parseInt(document.getElementById("forcedLatitude").value);
+    var givenLongitude = parseInt(document.getElementById("forcedLongitude").value);
+    
+    /*var weather = document.getElementById("forcedWeather").value;
+    var speedLimit = document.getElementById("forcedSpeed").value;
+    var roadSigns = document.getElementById("roadSignsS").value;
+    var roadShape = document.getElementById("roadShapes").value;*/
+    
+    var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<div id="bodyContent">'+
+                /*'<p><b>Weather:</b> ' + weather +
+                '<p><b>Speed Limit:</b> ' + speedLimit +
+                '<p><b>Road Signs:</b> '+ roadSigns +
+                '<p><b>Road Shape:</b> '+ roadShape +*/
+                '</div>';
+    
+    
+    var latArray = [40.7608, 40.8608];
+    var longArray = [-111.8910, -111.9910];
+      var givenCenter = {lat: 40.7608, lng: -111.8910};
+    
+    
+    latArray.push(givenLatitude);
+    longArray.push(givenLongitude);
+    
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: givenCenter
+      });
+    
+      for (i = 0; i < latArray.length; i++) {
+      placeMarker(latArray[i], longArray[i], map, contentString);
+      }
+    
+    }
+    //________________________________________________________________________________________________________________________________________________________________________________________________________
+    function stopInit() {
+    
+    var givenLatitude = parseInt(document.getElementById("stopLat").value);
+    var givenLongitude = parseInt(document.getElementById("stopLong").value);
+    
+    /*var weather = document.getElementById("forcedWeather").value;
+    var speedLimit = document.getElementById("forcedSpeed").value;
+    var roadSigns = document.getElementById("roadSignsS").value;
+    var roadShape = document.getElementById("roadShapes").value;*/
+    
+    var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<div id="bodyContent">'+
+                /*'<p><b>Weather:</b> ' + weather +
+                '<p><b>Speed Limit:</b> ' + speedLimit +
+                '<p><b>Road Signs:</b> '+ roadSigns +
+                '<p><b>Road Shape:</b> '+ roadShape +*/
+                '</div>';
+    
+    var latArray = [40.7608, 40.8608];
+    var longArray = [-111.8910, -111.9910];
+      var givenCenter = {lat: 40.7608, lng: -111.8910};
+    
+    
+    latArray.push(givenLatitude);
+    longArray.push(givenLongitude);
+    
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: givenCenter
+      });
+    
+      for (i = 0; i < latArray.length; i++) {
+      placeMarker(latArray[i], longArray[i], map, contentString);
+      }
+    
+    }
+    
+    //___________________________________________________________________________________________________________________________________________________________________________________________
+    function limitInit() {
+    
+    var givenLatitude = parseInt(document.getElementById("speedLat").value);
+    var givenLongitude = parseInt(document.getElementById("speedLong").value);
+    
+    /*var weather = document.getElementById("forcedWeather").value;
+    var speedLimit = document.getElementById("forcedSpeed").value;
+    var roadSigns = document.getElementById("roadSignsS").value;
+    var roadShape = document.getElementById("roadShapes").value;*/
+    
+    var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<div id="bodyContent">'+
+                /*'<p><b>Weather:</b> ' + weather +
+                '<p><b>Speed Limit:</b> ' + speedLimit +
+                '<p><b>Road Signs:</b> '+ roadSigns +
+                '<p><b>Road Shape:</b> '+ roadShape +*/
+                '</div>';
+    
+    
+    var latArray = [40.7608, 40.8608];
+    var longArray = [-111.8910, -111.9910];
+      var givenCenter = {lat: 40.7608, lng: -111.8910};
+    
+    
+    latArray.push(givenLatitude);
+    longArray.push(givenLongitude);
+    
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: givenCenter
+      });
+    
+      for (i = 0; i < latArray.length; i++) {
+      placeMarker(latArray[i], longArray[i], map, contentString);
+      }
+    
+    }
+    //_______________________________________________________________________________________________________________________________________________________________________________________________________
+    
+    function lightInit() {
+    
+    var givenLatitude = parseInt(document.getElementById("lightLat").value);
+    var givenLongitude = parseInt(document.getElementById("lightLong").value);
+    
+    /*var weather = document.getElementById("forcedWeather").value;
+    var speedLimit = document.getElementById("forcedSpeed").value;
+    var roadSigns = document.getElementById("roadSignsS").value;
+    var roadShape = document.getElementById("roadShapes").value;*/
+    
+    var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<div id="bodyContent">'+
+                /*'<p><b>Weather:</b> ' + weather +
+                '<p><b>Speed Limit:</b> ' + speedLimit +
+                '<p><b>Road Signs:</b> '+ roadSigns +
+                '<p><b>Road Shape:</b> '+ roadShape +*/
+                '</div>';
+    
+    
+    var latArray = [40.7608, 40.8608];
+    var longArray = [-111.8910, -111.9910];
+      var givenCenter = {lat: 40.7608, lng: -111.8910};
+    
+    
+    latArray.push(givenLatitude);
+    longArray.push(givenLongitude);
+    
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: givenCenter
+      });
+    
+      for (i = 0; i < latArray.length; i++) {
+      placeMarker(latArray[i], longArray[i], map, contentString);
+      }
+    
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    function driverPage() {
+    
+    var givenLatitude = parseInt(document.getElementById("forcedLatitude").value);
+    var givenLongitude = parseInt(document.getElementById("forcedLongitude").value);
+    
+    /*var weather = document.getElementById("forcedWeather").value;
+    var speedLimit = document.getElementById("forcedSpeed").value;
+    var roadSigns = document.getElementById("roadSignsS").value;
+    var roadShape = document.getElementById("roadShapes").value;*/
+    
+    var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<div id="bodyContent">'+
+                /*'<p><b>Weather:</b> ' + weather +
+                '<p><b>Speed Limit:</b> ' + speedLimit +
+                '<p><b>Road Signs:</b> '+ roadSigns +
+                '<p><b>Road Shape:</b> '+ roadShape +*/
+                '</div>';
+    
+    
+    var latArray = [40.7608, 40.8608];
+    var longArray = [-111.8910, -111.9910];
+      var givenCenter = {lat: 40.7608, lng: -111.8910};
+    
+    
+    latArray.push(givenLatitude);
+    longArray.push(givenLongitude);
+    
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: givenCenter
+      });
+    
+      for (i = 0; i < latArray.length; i++) {
+      placeMarker(latArray[i], longArray[i], map, contentString);
+      }
+    
+    }
+    
+    //___________________________________________________________________________________________________________________________________________________________________________________________
+    
+    function placeMarker(givenLat, givenLong, map, givenContent, givenIcons) {
+    
+            var infowindow = new google.maps.InfoWindow({
+              content: givenContent
+            });
+    
+            var marker = new google.maps.Marker({
+                position: {lat: givenLat, lng: givenLong},
+                map: map,
+                title: 'GIVEEEEN MEEEAAAPP',
+                draggable:true
+            });
+    
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+    
+    }
