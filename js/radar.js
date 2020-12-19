76a933c0dd6b5f1e3a196b9bb2c002b172805039
ayaanzhaque <ayaanzhaque@gmail.com>
2020-12-19 15:42:31 -0800

    added geofence code

--- js/radar.js
+++ js/radar.js
@@ -1,6 +1,23 @@
 import Http from '../http';
 import Navigator from '../navigator';
 
+Radar.initialize("prj_test_pk_...");
+
+      Radar.ipGeocode(function(err, result) {
+        if (err) {
+          console.error(err);
+
+          return;
+        }
+
+        if (result && result.address) {
+          console.log(result.address);
+
+          document.getElementById("country").innerHTML = "Your country is " +
+            result.address.countryFlag + " " + result.address.countryCode;
+        }
+      });
+
 class Geocoding {
   static async geocode(geocodeOptions={}) {
     const { query, layers } = geocodeOptions;
@@ -37,5 +54,22 @@ class Geocoding {
     return Http.request('GET', 'v1/geocode/ip', { ip });
   }
 }
+
+const geofence = async (lat, lng, radius, desc) => {
+  await fetch(
+    `https://api.radar.io/v1/geofences?description=${desc}&type=circle&coordinates=[${lng},${lat}]&radius=${radius}`,
+    {
+      headers: {
+        Authorization: 'prj_live_pk_9960fa9015ec1c672178a43fa62142afc16b6aed',
+      },
+      method: 'PUT',
+    },
+  );
+  const tag = desc.split('#')[0].trim();
+  const externalId = desc.split('#')[1];
+  return { tag, externalId };
+};
+
+export default geofence;
 
 export default Geocoding;
